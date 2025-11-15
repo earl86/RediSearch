b13e740f6d7a971c6c9a9e05af79eaa8c81e0043
nafraf <nafraf@gmail.com>
2025-11-15 15:35:42 -0500

    BuildMRCommand: Preserve WITHCOUNT/WITHOUTCOUNT

--- src/coord/dist_aggregate.c
+++ src/coord/dist_aggregate.c
@@ -85,6 +85,17 @@ static void buildMRCommand(RedisModuleString **argv, int argc, int profileArgs,
   // Numeric responses are encoded as simple strings.
   array_append(tmparr, "_NUM_SSTRING");
 
+  int argOffset = 0;
+  // Preserve WITHCOUNT/WITHOUTCOUNT flag from the original command
+  argOffset  = RMUtil_ArgIndex("WITHCOUNT", argv + 3 + profileArgs, argc - 3 - profileArgs);
+  if (argOffset != -1) {
+    array_append(tmparr, "WITHCOUNT");
+  }
+  argOffset  = RMUtil_ArgIndex("WITHOUTCOUNT", argv + 3 + profileArgs, argc - 3 - profileArgs);
+  if (argOffset != -1) {
+    array_append(tmparr, "WITHOUTCOUNT");
+  }
+
   // Add the index prefixes to the command, for validation in the shard
   array_append(tmparr, "_INDEX_PREFIXES");
   arrayof(HiddenUnicodeString*) prefixes = sp->rule->prefixes;
@@ -98,7 +109,7 @@ static void buildMRCommand(RedisModuleString **argv, int argc, int profileArgs,
   // Slots info will be added here
   uint32_t slotsInfoPos = array_len(tmparr);
 
-  int argOffset = RMUtil_ArgIndex("DIALECT", argv + 3 + profileArgs, argc - 3 - profileArgs);
+  argOffset = RMUtil_ArgIndex("DIALECT", argv + 3 + profileArgs, argc - 3 - profileArgs);
   if (argOffset != -1 && argOffset + 3 + 1 + profileArgs < argc) {
     array_append(tmparr, "DIALECT");
     array_append(tmparr, RedisModule_StringPtrLen(argv[argOffset + 3 + 1 + profileArgs], NULL));  // the dialect
